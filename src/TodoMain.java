a9cf8cca2b42fe8248dc27989e019238a7b119ab
Viktor Kemeny <kemeny.viktor@hotmail.com>
2017-04-06 16:35:44 +0200

    Remove function works properly now

--- src/TodoMain.java
+++ src/TodoMain.java
@@ -6,39 +6,35 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class TodoMain {
-  private final static String FILE_NAME = "../data.csv";
+  private final static String FILE_NAME = "../../../data.csv";
 
   public static void main(String[] args) {
     List<String> todoLines = readLinesFromFile();
 
     if (args.length == 0) {
       Commands.printUsage();
-
-    else if (args[0].equals("-l") && todoLines.size() != 0) {
-        for (int i = 0; i < todoLines.size(); i++) {
-          System.out.println(i + 1 + " - " + " " + todoLines.get(i));
-          writeToFile(todoLines);
-        }
-      } else if (args[0].equals("-l") && todoLines.size() == 0) {
-        System.out.println("No todos for today! Enjoy your day! :)");
+    } else if (args[0].equals("-l") && todoLines.size() != 0) {
+      for (int i = 0; i < todoLines.size(); i++) {
+        System.out.println(i + 1 + " - " + " " + todoLines.get(i));
         writeToFile(todoLines);
-      } else if (args[0].equals("-a")) {
-        todoLines.add("[ ] " + args[1]);
-        writeToFile(todoLines);
-      } else if (args[0].equals("-r")) {
-        todoLines.remove(args[1]);
-        writeToFile(todoLines);
-      } else if (args[0].equals("-c")) {
-        todoLines.remove(args[1]);
-        todoLines.add("[x] " + args[1]);
-        writeToFile(todoLines);
-      } else {
-        System.out.println("ERROR");
       }
+    } else if (args[0].equals("-l") && todoLines.size() == 0) {
+      System.out.println("No todos for today! Enjoy your day! :)");
 
+    } else if (args[0].equals("-a")) {
+      todoLines.add("[ ] " + args[1]);
+
+    } else if (args[0].equals("-r")) {
+      todoLines.remove(Integer.parseInt(args[1]) - 1);
+
+    } else if (args[0].equals("-c")) {
+      todoLines.remove(args[1]);
+      todoLines.add("[x] " + args[1]);
+    } else {
+      System.out.println("ERROR : Unsupported argument , please type a valid argument.");
     }
+    writeToFile(todoLines);
   }
-
 
   private static List<String> readLinesFromFile() {
     Path path = Paths.get(FILE_NAME);
