fa9499e85c037d07e870c2e8c63e5a0f02834616
Viktor Kemeny <kemeny.viktor@hotmail.com>
2017-04-06 12:23:47 +0200

    Started to make printUsage

---
+++ src/Commands.java
@@ -0,0 +1,15 @@
+public class Commands {
+
+}
+
+  public void printUsage() {
+    System.out.println("Java Todo application\n" +
+            "====================\n" +
+            "\n" +
+            "Command line arguments:\n" +
+            " -l   Lists all the tasks\n" +
+            " -a   Adds a new task\n" +
+            " -r   Removes an task\n" +
+            " -c   Completes an task\n");
+  }
+
